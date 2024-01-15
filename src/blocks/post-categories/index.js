0bb2945554221a2163184a6d80e21b866166f59d
Nur Hasan <nafismahmudayon@gmail.com>
2024-01-15 16:35:48 +0600

    terms query prams added

--- src/blocks/post-categories/index.js
+++ src/blocks/post-categories/index.js
@@ -7,20 +7,7 @@ const { addFilter } = wp.hooks;
 icon Positon Args
 */
 
-const iconPositonArgsPro = {
-    none: { label: 'Choose Position', value: '' },
-    beforeFronttext: { label: 'Before Front text', value: 'beforeFronttext' },
-    afterFronttext: { label: 'After Front text', value: 'afterFronttext' },
-    beforeItems: { label: 'Before Items', value: 'beforeItems' },
-    afterItems: { label: 'After Items', value: 'afterItems' },
-    beforeItem: { label: 'Before Each Items', value: 'beforeItem' },
-    afterItem: { label: 'After Each Items', value: 'afterItem' },
-};
 
-addFilter('iconPositonArgs', 'post-grid/iconPositonArgs', function (options) {
-
-    return iconPositonArgsPro;
-});
 
 
 
