103074f3aa4ddd88227ff6af5af80c8b458a2f81
unknown <nafismahmudayon@gmail.com>
2024-03-25 15:57:48 +0600

    pro update

--- src/blocks/global/index.js
+++ src/blocks/global/index.js
@@ -626,7 +626,7 @@ const termsQueryPramsBasic = {
 		id: "parent",
 		label: "Parent",
 		description:
-			"Add '$id' to add Parent term ID to retrieve direct-child terms of.",
+			"Add {ID} to add Parent term ID to retrieve direct-child terms of.",
 		longDescription: "Parent term ID to retrieve direct-child terms of.",
 	},
 
@@ -2896,7 +2896,7 @@ const breadcrumbElementsSourcePro = [
 		},
 		options: {
 			showSeparator: true,
-					isLink: true,
+			isLink: true,
 		},
 		styles: {
 			color: { Desktop: "" },
@@ -3481,7 +3481,6 @@ addFilter(
 	}
 );
 
-
 /*
 Progress Bar Block Filter Hook Start 
 */
@@ -3507,5 +3506,494 @@ addFilter(
 	"post-grid/progress-bar",
 	function (options) {
 		return progressBarSourcePro;
+	}
+);
+
+/*
+Woo Product Info Item Block Filter Hook Start 
+*/
+// woo-product-info-item objectTypes
+
+const wooProductInfoItemObjectTypesPro = {
+	weight: { label: "Weight", value: "weight" },
+	length: { label: "Length", value: "length" },
+	width: { label: "Width", value: "width" },
+	height: { label: "Height", value: "height" },
+	dimensions: { label: "Dimensions", value: "dimensions" },
+	taxonomy: { label: "Taxonomy", value: "taxonomy" },
+	meta: { label: "Custom Meta", value: "meta",},
+};
+
+addFilter(
+	"postGridWooProductInfoItemObjectTypes",
+	"post-grid/woo-product-info-item",
+	function (options) {
+		return wooProductInfoItemObjectTypesPro;
+	}
+);
+
+/*
+Post Query Block Filter Hook Start 
+*/
+// post-query queryPrams
+
+const postQueryQueryPramsPro = {
+	postType: {
+		value: ["post"],
+		multiple: false,
+		id: "postType",
+		label: "Post types",
+		description: "Select Post Types to Query",
+	},
+	s: {
+		value: "",
+		multiple: false,
+		id: "s",
+		label: "Keyword",
+		description: "Search keyword, ex: hello",
+	},
+	postStatus: {
+		value: [],
+		multiple: false,
+		id: "postStatus",
+		label: "Post status",
+		description: "Query post by post status",
+	},
+	order: {
+		value: "",
+		multiple: false,
+		id: "order",
+		label: "Order",
+		description: "Post query order",
+	},
+	orderby: {
+		value: [],
+		multiple: false,
+		id: "orderby",
+		label: "Orderby",
+		description: "Post query orderby",
+	},
+	metaKey: {
+		value: "",
+		multiple: false,
+		id: "metaKey",
+		label: "Meta fields key",
+		description: "Post query by meta fields key",
+	},
+
+	// Category Parameters
+	cat: {
+		value: "",
+		multiple: false,
+		id: "cat",
+		label: "Category ID",
+		description: "Post query by Category ID",
+	},
+	categoryName: {
+		value: "",
+		multiple: false,
+		id: "categoryName",
+		label: "Category Name",
+		description: "Post query by Category Name",
+	},
+	categoryAnd: {
+		value: [],
+		multiple: false,
+		id: "categoryAnd",
+		label: "Category And",
+		description: "Post query by Category IDs",
+	},
+	categoryIn: {
+		value: [],
+		multiple: false,
+		id: "categoryIn",
+		label: "Category In",
+		description: "Post query by Category IDs",
+	},
+	categoryNotIn: {
+		value: [],
+		multiple: false,
+		id: "categoryNotIn",
+		label: "Category Not In",
+		description: "Post query by excluded Category IDs",
+	},
+
+	// Tag Parameters
+
+	tag: {
+		val: "",
+		multiple: false,
+		id: "tag",
+		label: "Tags",
+		description: "Post query by Tag slug",
+	},
+	tagId: {
+		val: "",
+		multiple: false,
+		id: "tagId",
+		label: "Tag Id",
+		description: "Post query by Tag ID",
+	},
+	tagAnd: {
+		val: [],
+		multiple: false,
+		id: "tagAnd",
+		label: "Tag And",
+		description: "Post query by Tag Ids",
+	},
+	tagIn: {
+		val: [],
+		multiple: false,
+		id: "tagIn",
+		label: "Tag In",
+		description: "Post query by Tag ids",
+	},
+	tagNotIn: {
+		val: [],
+		multiple: false,
+		id: "tagNotIn",
+		label: "Tag Not In",
+		description: "Post query by excluded Tag ids",
+	},
+	tagSlugAnd: {
+		val: [],
+		multiple: false,
+		id: "tagSlugAnd",
+		label: "Tag Slug And",
+		description: "Post query by Tags slug",
+	},
+	tagSlugIn: {
+		val: [],
+		multiple: false,
+		id: "tagSlugIn",
+		label: "Tag Slug In",
+		description: "Post query by excluded Tags slug",
+	},
+
+	taxQuery: {
+		value: [],
+		multiple: false,
+		id: "taxQuery",
+		label: "Tax Query",
+		description: "Taxonomies query arguments",
+	},
+	taxQueryRelation: {
+		val: "OR",
+		multiple: false,
+		id: "taxQueryRelation",
+		label: "Tax Query Relation",
+		description: "Taxonomies query relation",
+	},
+
+	// // Date Parameters
+	dateQuery: {
+		value: [],
+		multiple: false,
+		id: "dateQuery",
+		label: "Date Query",
+		description: "Post query by date",
+	},
+	year: {
+		val: "",
+		multiple: false,
+		id: "year",
+		label: "Year",
+		description: "Post query by year",
+	},
+	monthnum: {
+		val: "",
+		multiple: false,
+		id: "monthnum",
+		label: "Month",
+		description: "Post query by month",
+	},
+	w: {
+		val: "",
+		multiple: false,
+		id: "w",
+		label: "Week",
+		description: "Post query by week",
+	},
+	day: {
+		val: "",
+		multiple: false,
+		id: "day",
+		label: "Day",
+		description: "Post query by day",
+	},
+	hour: {
+		val: "",
+		multiple: false,
+		id: "hour",
+		label: "Hour",
+		description: "Post query by hour",
+	},
+	minute: {
+		val: "",
+		multiple: false,
+		id: "minute",
+		label: "Miniute",
+		description: "Post query by miniute",
+	},
+	second: {
+		val: "",
+		multiple: false,
+		id: "second",
+		label: "Second",
+		description: "Post query by second",
+	},
+	m: {
+		val: "",
+		multiple: false,
+		id: "m",
+		label: "Month",
+		description: "Post query by month",
+	},
+
+	// // Author Parameters
+	author: {
+		val: "",
+		multiple: false,
+		id: "author",
+		label: "Author",
+		description: "Post query by Author ID",
+	},
+	authorName: {
+		val: "",
+		multiple: false,
+		id: "authorName",
+		label: "Author Name",
+		description: "Post query by Author Name",
+	},
+	authorIn: {
+		val: [],
+		multiple: false,
+		id: "authorIn",
+		label: "Author In",
+		description: "Post query by Author IDs",
+	},
+	authorNotIn: {
+		val: [],
+		multiple: false,
+		id: "authorNotIn",
+		label: "Author Not In",
+		description: "Post query by exluded Author IDs",
+	},
+
+	p: {
+		val: "",
+		multiple: false,
+		id: "p",
+		label: "Post id",
+		description: "Post query by single post id",
+	},
+	name: {
+		val: "",
+		multiple: false,
+		id: "name",
+		label: "Name",
+		description: "Post query by post slug",
+	},
+	pageId: {
+		val: "",
+		multiple: false,
+		id: "pageId",
+		label: "Page Id",
+		description: "Post query by single page id",
+	},
+	pagename: {
+		val: "",
+		multiple: false,
+		id: "pagename",
+		label: "Page name",
+		description: "Post query by page slug",
+	},
+	postParent: {
+		val: "",
+		multiple: false,
+		id: "postParent",
+		label: "Post Parent",
+		description: "Add {ID} to Post query by post parent id",
+	},
+	postParentIn: {
+		val: [],
+		multiple: false,
+		id: "postParentIn",
+		label: "Post Parent In",
+		description: "Post query by post parent ids",
+	},
+	postParentNotIn: {
+		val: [],
+		multiple: false,
+		id: "postParentNotIn",
+		label: "Post Parent Not In",
+		description: "Post query by excluded post parent ids",
+	},
+	postIn: {
+		val: [],
+		multiple: false,
+		id: "postIn",
+		label: "Post In",
+		description: "Post query by multiple post ids, comma separated.",
+	},
+	postNotIn: {
+		val: [],
+		multiple: false,
+		id: "postNotIn",
+		label: "Post Not In",
+		description: "Post query by excluded post ids",
+	},
+	postNameIn: {
+		val: [{ slug: "" }],
+		multiple: false,
+		id: "postNameIn",
+		label: "Post Name In",
+		description: "Post query by post slugs",
+	},
+
+	hasPassword: {
+		val: "",
+		multiple: false,
+		id: "hasPassword",
+		label: "Has Password",
+		description: "Post query for posts with passwords",
+	},
+	postPassword: {
+		val: "",
+		multiple: false,
+		id: "postPassword",
+		label: "Post Password",
+		description: "Post query for posts with particular passwords",
+	},
+
+	commentCount: {
+		val: { compare: "=", value: 10 },
+		multiple: false,
+		id: "commentCount",
+		label: "Comment Count",
+		description: "Post query by comment count",
+	},
+
+	nopaging: {
+		val: "",
+		multiple: false,
+		id: "nopaging",
+		label: "No Paging",
+		description: "Enable show all posts or use pagination",
+	},
+	postsPerPage: {
+		val: "",
+		multiple: false,
+		id: "postsPerPage",
+		label: "Posts Per Page",
+		description: "Number of post to show per page",
+	},
+	paged: {
+		val: "",
+		multiple: false,
+		id: "paged",
+		label: "Paged",
+		description: "Pagination start with",
+	},
+	offset: {
+		val: "",
+		multiple: false,
+		id: "offset",
+		label: "Offset",
+		description: "Number of post to displace or pass over",
+	},
+	postsPerArchivePage: {
+		val: "",
+		multiple: false,
+		id: "postsPerArchivePage",
+		label: "Posts Per Archive Page",
+		description: "",
+	},
+	ignoreStickyPosts: {
+		val: "",
+		multiple: false,
+		id: "ignoreStickyPosts",
+		label: "Ignore Sticky Posts",
+		description: "Ignore post from post query",
+	},
+
+	metaKey: {
+		val: "",
+		multiple: false,
+		id: "metaKey",
+		label: "Meta Key",
+		description: "Post query by custom field key",
+	},
+	metaValue: {
+		val: "",
+		multiple: false,
+		id: "metaValue",
+		label: "Meta Value",
+		description: "Post query by custom field value",
+	},
+	metaValueNum: {
+		val: "",
+		multiple: false,
+		id: "metaValueNum",
+		label: "Meta Value Num",
+		description: "Post query by custom field value for number types",
+	},
+	metaCompare: {
+		val: "",
+		multiple: false,
+		id: "metaCompare",
+		label: "Meta Compare",
+		description: "Meta query compare",
+	},
+	metaQuery: {
+		value: [],
+		multiple: false,
+		id: "metaQuery",
+		label: "Meta Query",
+		description: "Advance meta fields query",
+	},
+
+	perm: {
+		val: "readable",
+		multiple: false,
+		id: "perm",
+		label: "Perm",
+		description: "User permission parameter",
+	},
+	postMimeType: {
+		val: [],
+		multiple: false,
+		id: "postMimeType",
+		label: "Post Mime Type",
+		description: "Post query by allwed post mime types",
+	},
+	cacheResults: {
+		val: false,
+		multiple: false,
+		id: "cacheResults",
+		label: "Cache Results",
+		description: "Enable Post information cache",
+	},
+	updatePostMetaCache: {
+		val: false,
+		multiple: false,
+		id: "updatePostMetaCache",
+		label: "Update Post Meta Cache",
+		description: "Enable Post meta information cache",
+	},
+	updatePostTermCache: {
+		val: false,
+		multiple: false,
+		id: "updatePostTermCache",
+		label: "Update Post Term Cache",
+		description: "Enable Post term information cache",
+	},
+};
+
+addFilter(
+	"postGridPostQueryQueryPramsPro",
+	"post-grid/post-query",
+	function (options) {
+		return postQueryQueryPramsPro;
 	}
 );
