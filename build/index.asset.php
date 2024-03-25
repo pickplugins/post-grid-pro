7d35f91e3350c8cd9a66a3e1a0788a2ed19c880d
PickPlugins <32812881+pickplugins@users.noreply.github.com>
2024-03-25 11:35:46 +0600

    Branch Commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'e3f48dd13b36588aca17');
+<?php return array('dependencies' => array('wp-element'), 'version' => '844cdd1c71eef937db0a');
